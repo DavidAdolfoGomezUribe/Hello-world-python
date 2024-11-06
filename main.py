7e3ac669d52d1dcfa01c845dac89ba0b1a785dc6
DavidAdolfoGomezUribe <96323893+DavidAdolfoGomezUribe@users.noreply.github.com>
2024-11-06 11:11:14 -0500

    Estructura del bucle for

--- main.py
+++ main.py
@@ -1,16 +1,3 @@
-fullName = input("Enter your name: ")
-age = int(input("Enter your age: "))
-gender = input("Enter your gender posibble options : M or W  ")
-height = float(input("altura"))
-
-
-
-
-print("""
-      hola, usuario {fullname}. ¿como estas? Te saluda Python. Gracias por compartir tu edad {age}; no sabia que eres un humano de tipo {gender}
-      """)
-
-  
-  
-#last line of code 
+for value in ["David","Adolfo","Gomez"]:
+    print(value)
  
